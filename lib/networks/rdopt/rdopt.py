3a9725f7e9ff5aca2f8be34f200e6e049f076df2
NiPeiyuan <113538796+NiPeiyuan@users.noreply.github.com>
2022-09-26 21:55:16 +0800

    Update rdopt.py

--- lib/networks/rdopt/rdopt.py
+++ lib/networks/rdopt/rdopt.py
@@ -59,6 +59,18 @@ initialbias=initialbias_dict[cfg.cls_type]
 face_num=face_num_dict[cfg.cls_type]
 savednumpy=[]
 savedindex=[]
+import tensorrt as trt
+
+logger = trt.Logger(trt.Logger.INFO)
+from torch2trt import TRTModule
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+
+f = open('./bestresult/'+cfg.cls_type+'/'+cfg.cls_type+'.trt', "rb")
+runtime = trt.Runtime(logger)
+engine = runtime.deserialize_cuda_engine(f.read())  
+trt_model = TRTModule(engine, input_names=['input'], output_names=['output1', 'output2', 'output3', 'output4', 'output5'])
 
 class RDOPT(nn.Module):
     def __init__(self):
@@ -160,15 +172,15 @@ class RDOPT(nn.Module):
 
     def process_siamese(self,data_i, featuresrequired=False):
  
+            pred_1, pred_2, pred_3, pred_4 , features = trt_model(data_i)
+            
+            pred_i = {'feature_maps': [pred_1,pred_2]}
+            pred_i['confidences'] = [pred_3,pred_4]
 
-            pred_i, features = self.UNet_(data_i)  
-  
             if featuresrequired:
                 return pred_i, features
             else:
                 return pred_i
-
-
 
 
 
